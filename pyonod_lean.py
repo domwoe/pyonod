d417aa5f3d08e173eecfb4c20f1128ab877c251a
Wörner  Dominic <dwoerner@ethz.ch>
2014-05-27 15:22:26 +0200

    added comments

--- pyonod_lean.py
+++ pyonod_lean.py
@@ -14,11 +14,24 @@ password = 'burger89]crew'
 DSN = 'dbname=%s host=%s user=%s password=%s' % (dbname, host, user, password)
 
 
+###########################################################
+###########  Rules and Threshold Definitions  #############
+###########################################################
+
+co2_arrival_thresh = 50
+co2_departure_thres = 25
+
+###########################################################
+
+# Computes the moving average of vector a taking n elements
 def moving_average(a, n=3) :
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
+# Inserts an event (1=arrival,0=departure) into the database as a sensor called onlinePresence (unitTypeId=17).
+# If there is no such sensor defined in a room it will be created.
+# Inserts only if last event differs from current event.
 def write_event(locId,event):
     curs.execute("SELECT m.\"reading\", m.\"sensorId\" FROM \"Measurements\" m, \"Sensors\" s WHERE m.\"sensorId\" = s.\"sensorId\" AND s.\"unitTypeId\"=17 AND s.\"locId\"=%s ORDER BY m.timestamp DESC LIMIT 1;" % locId )
     record  = curs.fetchone()
@@ -42,6 +55,9 @@ conn = psycopg2.connect(DSN)
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
 curs = conn.cursor()
+
+# Subscribe to measurement channel to get notified when
+# a new measurement is inserted into the database
 curs.execute("LISTEN measurement;")
 
 print "Waiting for notifications on channel 'measurement'"
@@ -64,10 +80,10 @@ while 1:
             	m = moving_average(measurements)
             	# Decide of levels are rising
             	if np.mean(np.diff(m)) > 0:
-            		if max(m) - min(m) > 50:	 
+            		if max(m) - min(m) > co2_arrival_thresh:	 
            			    print "arrival in locId "+locId
                         write_event(locId,1)
             	else:
-            		if max(m) - min(m) > 25:
+            		if max(m) - min(m) > co2_departure_thres:
             			print "departure in locId "+locId
-                        write_event(locId,0)	
+                        write_event(locId,0)
